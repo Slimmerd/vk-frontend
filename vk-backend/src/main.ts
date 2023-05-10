a8516ecd7e2f8d9802e7fd1f33fa7d7b33b9cfa6
Daniil Silin <daniil_silin@mail.ru>
2023-05-10 19:24:07 +0100

    Test

--- vk-backend/src/main.ts
+++ vk-backend/src/main.ts
@@ -6,8 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // allowedHeaders: '*',
-    origin: 'https://vk-frontend.vercel.app/',
+    allowedHeaders: '*',
+    origin: '*',
+    // origin: 'https://vk-frontend.vercel.app/',
     credentials: true,
   });
   app.use(cookieParser());
